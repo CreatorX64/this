cfea1cac75c2f274dcdabcfa10be869b434b5b9c
Hakan Güçlü <creatorX64@gmail.com>
2021-08-08 16:14:49 +0300

    update: widgets

--- this.buffer/watch-react/projects/12-understanding-hooks/widgets/src/components/App.js
+++ this.buffer/watch-react/projects/12-understanding-hooks/widgets/src/components/App.js
@@ -1,28 +1,28 @@
 import React from "react";
-import Accordion from "./Accordion";
+// import Accordion from "./Accordion";
+import Search from "./Search";
 
-const items =
-[
-  {
-    title: "What is React?",
-    content: "React is a frontend JavaScript library."
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JavaScript library among engineers."
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components."
-  }
-];
+// const items =
+// [
+//   {
+//     title: "What is React?",
+//     content: "React is a frontend JavaScript library."
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JavaScript library among engineers."
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components."
+//   }
+// ];
 
 const App = () =>
 {
   return (
     <div>
-      <br />
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
