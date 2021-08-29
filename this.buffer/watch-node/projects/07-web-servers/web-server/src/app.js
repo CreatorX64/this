8aca08c476930a9b3aeefdbc2dfca9e0f5f00a60
Hakan Güçlü <creatorX64@gmail.com>
2021-08-29 16:09:09 +0300

    update section 7

--- this.buffer/watch-node/projects/07-web-servers/web-server/src/app.js
+++ this.buffer/watch-node/projects/07-web-servers/web-server/src/app.js
@@ -1,21 +1,67 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
+import hbs from "hbs";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello express!");
-});
+// Define paths for Express configuration.
+const execDirPath = path.dirname(fileURLToPath(import.meta.url));
+const pubDirPath = path.join(execDirPath, "..", "public");
+const viewsPath = path.join(execDirPath, "..", "templates", "views");
+const partialsPath = path.join(execDirPath, "..", "templates", "partials");
 
-app.get("/help", (req, res) => {
-  res.send("Help page");
+// Setup handlebars engine and views location.
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve.
+app.use(express.static(pubDirPath));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Hakan Güçlü"
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About");
+  res.render("about", {
+    title: "About",
+    name: "Hakan Güçlü"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Hakan Güçlü",
+    helpText: "This is some helpful text."
+  });
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Your weather");
+  res.send({
+    forecast: "Sunny day",
+    location: "Istanbul, Turkey"
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Hakan Güçlü",
+    errorMessage: "Help article not found."
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Hakan Güçlü",
+    errorMessage: "Page not found."
+  });
 });
 
 app.listen(3000, () => {
