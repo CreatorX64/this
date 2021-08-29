1f63df45b07258b683b655e47850cb63d164bfd4
Hakan Güçlü <creatorX64@gmail.com>
2021-08-29 10:13:25 +0300

    add section 7

---
+++ this.buffer/watch-node/projects/07-web-servers/weather-app/app.js
@@ -0,0 +1,24 @@
+import { forecast } from "./utils/forecast.js";
+import { geocode } from "./utils/geocode.js";
+
+const address = process.argv[2];
+
+if (!address) {
+  console.log("Please provide a location.");
+  process.exit(0);
+}
+
+geocode(address, (error, { latitude, longitude, location } = {}) => {
+  if (error) {
+    return console.log(err);
+  }
+
+  forecast(latitude, longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(err);
+    }
+
+    console.log(location);
+    console.log(forecastData);
+  });
+});
