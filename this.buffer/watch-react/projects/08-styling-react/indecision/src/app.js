29a1715d6200d2713289c0f495ec1462b9f52481
Hakan Guclu <creatorX64@gmail.com>
2021-09-08 21:09:04 +0300

    udpate section 8

--- this.buffer/watch-react/projects/08-styling-react/indecision/src/app.js
+++ this.buffer/watch-react/projects/08-styling-react/indecision/src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { APP_ELEM_SELECTOR } from "./static";
 import { IndecisionApp } from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 ReactDOM.render(<IndecisionApp />, document.querySelector(APP_ELEM_SELECTOR));
--- this.buffer/watch-react/projects/08-styling-react/indecision/src/components/Header.js
+++ this.buffer/watch-react/projects/08-styling-react/indecision/src/components/Header.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export const Header = (props) => (
   <div className="header">
-    <h1 className="header__title">{props.title}</h1>
-    {props.subTitle && <h2 className="header__sub-title">{props.subTitle}</h2>}
+    <div className="container">
+      <h1 className="header__title">{props.title}</h1>
+      {props.subTitle && (
+        <h2 className="header__sub-title">{props.subTitle}</h2>
+      )}
+    </div>
   </div>
 );
 
