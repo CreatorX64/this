bdaeb01bcec4adc79cbf95640c23c25a9f4c7851
Hakan Guclu <creatorX64@gmail.com>
2021-09-08 14:55:08 +0300

    add section 6

---
+++ this.buffer/watch-react/projects/06-webpack/indecision/src/components/IndecisionApp.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Action } from "./Action";
+import { AddOption } from "./AddOption";
+import { Header } from "./Header";
+import { Options } from "./Options";
+
+export class IndecisionApp extends Component {
+  state = { options: [] };
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  };
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => option !== optionToRemove)
+    }));
+  };
+
+  handlePick = () => {
+    const randomIndex = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomIndex];
+    alert(option);
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists";
+    }
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option)
+    }));
+  };
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem("options");
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all. The app will fall back to using empty array.
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem("options", json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  render() {
+    const subTitle = "Put your life in the hands of a computer";
+
+    return (
+      <div>
+        <Header subTitle={subTitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption handleAddOption={this.handleAddOption} />
+      </div>
+    );
+  }
+}
