0aaf5f57a6c143dea22988e5807796c8011dab73
Hakan Güçlü <creatorX64@gmail.com>
2021-09-28 14:01:54 +0300

    Create server, integrate graphql and mongo

--- this.buffer/watch-react/project/server/app.js
+++ this.buffer/watch-react/project/server/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import mongoose from "mongoose";
 import { schema } from "./schema/schema.js";
 
 const app = express();
 
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+mongoose.connection.once("open", function () {
+  console.log("Connected to database");
+});
+
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
-app.listen(4000, () => {
+app.listen(4000, function () {
   console.log("Listening for requests on port 4000");
 });
