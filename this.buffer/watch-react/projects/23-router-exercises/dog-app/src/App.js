9d98e66ae532de26ce130cdf067d5a75babd729d
Hakan Guclu <creatorX64@gmail.com>
2021-10-15 17:04:37 +0300

    Add section 23

---
+++ this.buffer/watch-react/projects/23-router-exercises/dog-app/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "./App.css";
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Dog App</h1>
+      </div>
+    );
+  }
+}
