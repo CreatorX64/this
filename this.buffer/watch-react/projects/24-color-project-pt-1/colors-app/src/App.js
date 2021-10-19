fc3e111579a3187df6fa8a754d97cbc2bf4fced9
Hakan Guclu <creatorX64@gmail.com>
2021-10-19 19:46:10 +0300

    Update section 24

--- this.buffer/watch-react/projects/24-color-project-pt-1/colors-app/src/App.js
+++ this.buffer/watch-react/projects/24-color-project-pt-1/colors-app/src/App.js
@@ -2,12 +2,13 @@ import { Component } from "react";
 import { Palette } from "./Palette";
 import { seedPalettes } from "./seedPalettes";
 import "./App.css";
+import { generatePalette } from "./colorHelpers";
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <Palette {...seedPalettes[2]} />
+        <Palette palette={generatePalette(seedPalettes[4])} />
       </div>
     );
   }
