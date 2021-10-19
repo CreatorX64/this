b12563abedf2412b53219215a4923133b6e9c6e1
Hakan Guclu <creatorX64@gmail.com>
2021-10-20 00:46:52 +0300

    Update section 25

--- this.buffer/watch-react/projects/25-color-project-pt-2/colors-app/src/App.js
+++ this.buffer/watch-react/projects/25-color-project-pt-2/colors-app/src/App.js
@@ -1,14 +1,37 @@
 import { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import { Palette } from "./Palette";
+import { PaletteList } from "./PaletteList";
 import { seedPalettes } from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
 
 export class App extends Component {
+  findPalette(id) {
+    return seedPalettes.find((palette) => palette.id === id);
+  }
+
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedPalettes[4])} />
-      </div>
+      <Switch>
+        <Route
+          path="/"
+          render={() => <PaletteList palettes={seedPalettes} />}
+          exact
+        />
+        <Route
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedPalettes[4])} />
+      // </div>
     );
   }
 }
--- this.buffer/watch-react/projects/25-color-project-pt-2/colors-app/src/Navbar.js
+++ this.buffer/watch-react/projects/25-color-project-pt-2/colors-app/src/Navbar.js
@@ -1,16 +1,36 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton, MenuItem, Select, Snackbar } from "@mui/material";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex", isSnackbarOpen: false };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
+  }
+
+  handleFormatChange(event) {
+    this.setState({ format: event.target.value, isSnackbarOpen: true });
+    this.props.handleChange(event.target.value);
+  }
+
+  closeSnackbar() {
+    this.setState({ isSnackbarOpen: false });
+  }
+
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
 
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#!">React Color Picker</a>
+          <Link to="/">React Color Picker</Link>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
@@ -25,6 +45,35 @@ export class Navbar extends Component {
             />
           </div>
         </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgb(255, 255, 255, 1.0)</MenuItem>
+          </Select>
+        </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.isSnackbarOpen}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{ "aria-describedby": "message-id" }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
