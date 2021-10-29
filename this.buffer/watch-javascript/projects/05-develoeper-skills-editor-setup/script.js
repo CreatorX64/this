867dd0569bf11dc8d0ead5551d85e0ca374ab210
Hakan Guclu <creatorX64@gmail.com>
2021-10-29 22:11:14 +0300

    Update section 5

--- this.buffer/watch-javascript/projects/05-develoeper-skills-editor-setup/script.js
+++ this.buffer/watch-javascript/projects/05-develoeper-skills-editor-setup/script.js
@@ -12,6 +12,7 @@ if (x === 23) {
 const calcAge = (birthYear) => 2037 - birthYear;
 */
 
+/*
 //-- Problem: We work for a company building a smart home thermometer. Our most
 // recent task is this: "Given an array of temperatures of one day, calculate the
 // temperature amplitude. Keep in mind that sometimes there might be a sensor error."
@@ -90,3 +91,34 @@ function calcTempAmplitudeNew(temps1, temps2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+/*
+//-- Debugging with console and breakpoints
+
+function measureKelvin() {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // value: prompt("Degrees celsius:"),
+    // C) Fix
+    value: Number(prompt("Degrees celsius:"))
+  };
+
+  // B) Find
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  // We can place breakpoints in our source code using devtools "Sources" tab. Alternatively, you can active the debugger by typing:
+  // debugger;
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+}
+
+// A) Identify
+console.log(measureKelvin());
+*/
