585872c7380039bf5e656b5f43481513d647c0f6
Hakan Guclu <creatorX64@gmail.com>
2021-11-10 22:50:15 +0300

    add section 11

---
+++ this.buffer/watch-typescript/projects/11-using-webpack-with-ts/understanding-ts/src/decorators/bind.ts
@@ -0,0 +1,13 @@
+export function Bind(
+  _1: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
+  return {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return descriptor.value.bind(this);
+    }
+  };
+}
