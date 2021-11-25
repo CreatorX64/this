04cc671bda0892ca10dd6dcb471761040d0b9298
Hakan Guclu <creatorX64@gmail.com>
2021-11-25 18:38:31 +0300

    Add Tailwind intro course

---
+++ this.study/watchable/tailwind/Tailwind CSS. From Zero to Production by Tailwind Labs/up-and-running/src/App.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { DestinationCard } from "./components/DestinationCard";
+import { popularDestinations } from "./data/seed";
+
+export const App = () => {
+  return (
+    <div className="bg-gray-200">
+      <div className="grid bg-gray-100 lg:grid-cols-2 2xl:grid-cols-5">
+        <div className="max-w-md mx-auto px-8 py-12 sm:max-w-xl lg:max-w-full lg:px-12 lg:py-24 xl:mr-0 2xl:col-span-2">
+          <div className="xl:max-w-xl">
+            <img
+              className="h-10"
+              src="/img/logo-brand.svg"
+              alt="Workcation logo"
+            />
+            <img
+              className="mt-6 rounded-lg shadow-xl sm:w-full sm:h-64 sm:mt-8 sm:object-cover sm:object-center lg:hidden"
+              src="/img/beach-work.jpg"
+              alt="Woman workcationing on the beach"
+            />
+            <h1 className="mt-6 text-2xl font-headline tracking-tight font-semibold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+              You can work from anywhere.
+              <br className="hidden lg:inline" />
+              <span className="text-brand"> Take advantage of it.</span>
+            </h1>
+            <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+              Workcation helps you find work-friendly rentals in beautiful
+              locations so you can enjoy some nice weather even when you're not
+              on vacation.
+            </p>
+            <div className="mt-4 space-x-1 sm:mt-6">
+              <a
+                className="btn btn-primary shadow-lg transform transition hover:-translate-y-0.5"
+                href="#!"
+              >
+                Book your escape
+              </a>
+              <a className="btn btn-secondary" href="#!">
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden lg:block 2xl:col-span-3">
+          <img
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src="/img/beach-work.jpg"
+            alt="Woman workcationing on the beach"
+          />
+        </div>
+      </div>
+      <div className="max-w-md mx-auto px-8 py-8 sm:max-w-xl lg:max-w-6xl lg:px-12">
+        <h2 className="text-xl text-gray-900">Popular destinations</h2>
+        <p className="mt-2 text-gray-600">
+          A selection of great work-friendly cities with lots to see and
+          explore.
+        </p>
+        <div className="grid gap-6 mt-6 lg:grid-cols-2 xl:grid-cols-3">
+          {popularDestinations.map((destination) => (
+            <DestinationCard key={destination.city} destination={destination} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
