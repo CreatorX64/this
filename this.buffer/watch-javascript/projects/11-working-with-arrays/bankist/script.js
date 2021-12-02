e13721c3b3f8a9ff764b7faa70e583f805e296d6
Hakan Guclu <creatorX64@gmail.com>
2021-12-02 06:10:56 +0300

    Add section 11

--- this.buffer/watch-javascript/projects/11-working-with-arrays/bankist/script.js
+++ this.buffer/watch-javascript/projects/11-working-with-arrays/bankist/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
 // Data
 const account1 = {
   owner: "Jonas Schmedtmann",
@@ -57,50 +61,81 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-/////////////////////////////////////////////////
-// LECTURES
-/////////////////////////////////////////////////
-
-//-- Simple array methods
 /*
+//-- Simple array methods
+
 let arr = ["a", "b", "c", "d", "e"];
 
 // Slice
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
 
-// Splice: Returns removed elements as new array, mutates original array.
-// console.log(arr.splice(2));
+console.log(arr.slice(2)); // ['c', 'd', 'e']
+console.log(arr.slice(2, 4)); // ['c', 'd']
+console.log(arr.slice(-2)); // ['d', 'e']
+console.log(arr.slice(-1)); // ['e']
+console.log(arr.slice(1, -1)); // ['b', 'c', 'd']
+console.log(arr.slice()); // Shallow copy
+console.log([...arr]); // Shallow copy
+
+// Splice
+
+// console.log(arr.splice(2)); // ['c', 'd', 'e']
+// console.log(arr); // ['a', 'b'] Original array is mutated
+
+// arr.splice(-1); // Remove & return the last element
 // console.log(arr);
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
 
-// Reverse: Mutates original array.
-arr = ["a", "b", "c", "d", "e"];
+// arr.splice(1, 2);
+// console.log(arr); // ['a', 'd', 'e']
+
+// Reverse
+
 const arr2 = ["j", "i", "h", "g", "f"];
-console.log(arr2.reverse());
+
+console.log(arr2.reverse()); // Mutates original array
 console.log(arr2);
 
-// Concat: Doesn't mutate original array.
+// Concat
+
+arr = ["a", "b", "c", "d", "e"];
 const letters = arr.concat(arr2);
 console.log(letters);
 console.log([...arr, ...arr2]);
 
 // Join
-console.log(letters.join(" - "));
+
+console.log(letters.join(" — "));
 */
 
-//-- Loop arrays: forEach
 /*
+//-- The at() method (ES2022): Useful for accessing single array items relative
+// to array end, or using with method chaining.
+
+const arr = [23, 11, 64];
+
+// Getting the last element
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+// Getting an arbitrary element
+
+console.log(arr[0]);
+console.log(arr.at(0));
+console.log(arr.at(-2));
+
+// Also works with strings
+
+console.log("jonas".at(-1));
+*/
+
+/*
+//-- Looping arrays: forEach: One notable difference from classical loops or
+// for...of loop is that you cannot break; or continue; inside a forEach loop.
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+// for (const movement of movements) {
 for (const [i, mov] of movements.entries()) {
   if (mov > 0) {
     console.log(`Movement ${i + 1}: You deposited ${mov}`);
@@ -109,9 +144,9 @@ for (const [i, mov] of movements.entries()) {
   }
 }
 
-console.log("---- forEach ----");
+console.log("--------");
 
-// Note: "continue" and "break" don't work in forEach.
+// forEach() params: Current array item, current item index, whole array.
 movements.forEach((mov, i, arr) => {
   if (mov > 0) {
     console.log(`Movement ${i + 1}: You deposited ${mov}`);
@@ -132,17 +167,22 @@ const currencies = new Map([
   ["GBP", "Pound sterling"]
 ]);
 
+// forEach params on a Map correspond to the params of the array version.
 currencies.forEach((value, key, map) => {
   console.log(`${key}: ${value}`);
 });
 
 // Set
 
-// const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
-const currenciesUnique = new Set(currencies.keys());
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
 console.log(currenciesUnique);
 
-currenciesUnique.forEach((value, _, map) => {
-  console.log(`${value}`);
+// forEach params on Set: "value" and "key" hold the same value because a Set
+// doesn't have keys or indexes, so there's no value that would make sense
+// for the key parameter. That's why the value is passed to both of the
+// first 2 parameters so that the method signature would be the same as it
+// is on arrays and Maps.
+currenciesUnique.forEach((value, key, set) => {
+  console.log(`${key}: ${value}`);
 });
 */
