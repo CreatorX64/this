8acfcb7af0cbde21c4e03eabe2c2f729d66ee5fc
Hakan Guclu <creatorX64@gmail.com>
2022-01-14 22:02:35 +0300

    Update ch 5

--- this.buffer/read-typescript/projects/05-using-ts-compiler/tools/src/index.ts
+++ this.buffer/read-typescript/projects/05-using-ts-compiler/tools/src/index.ts
@@ -1,5 +1,15 @@
-function printMessage(msg: string): void {
-  console.log(`Message: ${msg}`);
-}
+import { sum } from "./calc";
 
-printMessage("Hello, TypeScript");
+let printMessage = (msg: string): void => console.log(`Message: ${msg}`);
+
+let message = "Hello, TypeScript";
+printMessage(message);
+
+// Following code will generate a compiler error when "target" is "ES5"
+// let data = new Map();
+// data.set("Bob", "London");
+// data.set("Alice", "Paris");
+// data.forEach((val, key) => console.log(`${key}: ${val}`));
+
+let total = sum(100, 200, 300);
+console.log(`Total: ${total}`);
