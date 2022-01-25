d6795bf47aedb03de42fc7ede1c862b5899cea86
Hakan Guclu <creatorX64@gmail.com>
2022-01-25 22:00:10 +0300

    Add pt 1

---
+++ this.buffer/read-react/projects/01-fundamentals-of-react/hacker-stories/src/App.js
@@ -0,0 +1,45 @@
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    numComments: 3,
+    points: 4,
+    objectId: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    numComments: 2,
+    points: 5,
+    objectId: 1
+  }
+];
+
+function App() {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+
+      <hr />
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>{" "}
+            <span>{item.author}</span> <span>{item.numComments}</span>{" "}
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
