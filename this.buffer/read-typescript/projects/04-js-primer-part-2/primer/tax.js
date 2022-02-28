c1ce8fb1d43e6dbde1bdaa1a1fd63ae4ceccc68a
Hakan Guclu <creatorX64@gmail.com>
2022-02-28 12:57:05 +0300

    remove ts book files

--- this.buffer/read-typescript/projects/04-js-primer-part-2/primer/tax.js
+++
@@ -1,7 +0,0 @@
-export default function calcTax(price) {
-  return Number(price) * 1.2;
-}
-
-export function calcTaxandSum(...prices) {
-  return prices.reduce((total, p) => (total += calcTax(p)), 0);
-}
