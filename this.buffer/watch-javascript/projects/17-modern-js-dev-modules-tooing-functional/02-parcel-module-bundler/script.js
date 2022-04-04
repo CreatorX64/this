afadf26a7a132add042ea548fdb018eb2e29921f
Hakan Guclu <creatorX64@gmail.com>
2022-04-05 00:14:21 +0300

    update sect 17

--- this.buffer/watch-javascript/projects/17-modern-js-dev-modules-tooing-functional/02-parcel-module-bundler/script.js
+++ this.buffer/watch-javascript/projects/17-modern-js-dev-modules-tooing-functional/02-parcel-module-bundler/script.js
@@ -29,3 +29,45 @@ console.log({ stateDeepClone });
 if (module.hot) {
   module.hot.accept();
 }
+
+//-- Parcel transpiling demo
+
+class Person {
+  #greeting = "Hey";
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person("jonas");
+console.log(jonas);
+
+console.log(cart.find((item) => item.quantity >= 2));
+Promise.resolve("TEST").then((x) => console.log(x));
+
+//-- Polyfilling: Two packages are used for polyfilling your code:
+//     - "core-js" to polyfill ECMAScript features
+//     - "regenerator-runtime/runtime" to use transpiled generator functions
+
+// By default, Parcel uses Babel to "transpile" (convert) JavaScript syntax to
+// a default widely accessible JavaScript syntax. However, there's another step
+// that's required if you want to support older brwoser and that step is called
+// "polyfilling". Babel can transpile language features like arrow functions
+// without a problem because it has a direct counterpart, reguler functions.
+// However, to convert features like array find() method, a polyfill library
+// is needed. The polyfill library includes a re-created version of the polyfilled
+// features (like find() method) and make it available in the bundle so that the
+// code can use it. "core-js" is such a library which you can import to polyfill
+// language features used in your modules.
+import "core-js/stable";
+
+// We can cherry-pick what to polyfill by importing only that specific
+// implementation. This can take longer time to prepare, but if your bundle size
+// is not an issue then you don't need to do it like this (normally it's rarely
+// done like this)
+// import "core-js/stable/array/find";
+// import "core-js/stable/promise";
+
+// Polyfilling async functions
+import "regenerator-runtime/runtime";
