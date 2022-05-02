1db75f8b4227d94cc193edcb94531be683ea9be4
Hakan Guclu <creatorX64@gmail.com>
2022-05-02 18:28:16 +0300

    add sect 12

---
+++ this.buffer/watch-firebase/projects/12-react-context-and-reducers/cooking-ninja/src/components/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "@/components/Navbar";
+import Home from "@/pages/home/Home";
+import Create from "@/pages/create/Create";
+import Search from "@/pages/search/Search";
+import Recipe from "@/pages/recipe/Recipe";
+import styles from "@/components/App.module.css";
+
+const App = () => (
+  <div className={styles.app}>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/recipes/:id" element={<Recipe />} />
+    </Routes>
+  </div>
+);
+
+export default App;
---
+++ this.buffer/watch-firebase/projects/12-react-context-and-reducers/cooking-ninja/src/pages/recipe/Recipe.jsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import useFetch from "@/hooks/useFetch";
+import styles from "@/pages/recipe/Recipe.module.css";
+
+const Recipe = () => {
+  const { id } = useParams();
+  const {
+    data: recipe,
+    isPending,
+    error
+  } = useFetch(`http://localhost:8080/recipes/${id}`);
+
+  return (
+    <div className={styles.recipe}>
+      {error && <p className="error">{error}</p>}
+
+      {isPending && <p className="loading">Loading...</p>}
+
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className={styles.method}>{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Recipe;
