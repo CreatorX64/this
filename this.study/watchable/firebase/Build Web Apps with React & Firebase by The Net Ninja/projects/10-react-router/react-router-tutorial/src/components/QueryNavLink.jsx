626eb74ece4a995f7035185346604b10418524d8
Hakan Guclu <creatorX64@gmail.com>
2022-06-16 22:29:25 +0300

    add firebase course files

--- this.study/watchable/firebase/Build Web Apps with React & Firebase by The Net Ninja/projects/10-react-router/react-router-tutorial/src/components/QueryNavLink.jsx
+++
@@ -1,6 +0,0 @@
-import { useLocation, NavLink } from "react-router-dom";
-
-export const QueryNavLink = ({ to, ...props }) => {
-  const location = useLocation();
-  return <NavLink to={to + location.search} {...props} />;
-};
--- this.study/watchable/firebase/Build Web Apps with React & Firebase by The Net Ninja/projects/17-project-management-site/the-dojo/src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Avatar.module.css";
-
-export function Avatar({ src, className }) {
-  return (
-    <div className={`${styles.avatar} ${className ?? ""}`}>
-      <img src={src} alt="User avatar" />
-    </div>
-  );
-}
